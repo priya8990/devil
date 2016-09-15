f6d2b1b2370d8d23f24bb6c543b9b1d040456f07
anjankumarhn <anjankumar@artrium.me>
2016-09-15 13:13:22 +0530

    Added Capistrano script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,8 @@
 set :application, 'devil'
 set :repo_url, 'git@github.com:priya8990/devil.git'
 
-
+set :rbenv_ruby, '2.2.3'
+set :rbenv_path, '/home/demo/.rbenv'
 set :format, :pretty
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
